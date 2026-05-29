8828afb4591aad40843bd5c3309271f183c41064
Jhonatan Luiz <jhonatan@example.com>
2026-05-28 21:08:15 -0300

    chore: save sanity studio appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: 'up3mwksb',
     dataset: 'production'
   },
+  studioHost: 'mg-modas-feminina',
   deployment: {
+    appId: 'adydrrys8u2y0kb3evmyb6rq',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
